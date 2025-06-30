65c8bf9de3991cfcfc7056b1b6c876f8d48663c2
thenick775 <nvancisedev@gmail.com>
2025-06-29 20:32:27 -0700

    feat: pwa assets+generation

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,38 @@ import { VitePWA } from 'vite-plugin-pwa';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   base: './',
-  plugins: [prefresh(), visualizer(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    prefresh(),
+    visualizer(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
